066a60777744e8bff177459208de8dec285ce93c
Lucas Capdevila <lcapdevila60@gmail.com>
2024-01-30 20:23:26 -0300

    se empieza a trabajar en componente Input

---
+++ src/components/Input.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Form } from 'react-bootstrap'
+
+const Input = () => {
+  return (
+    <Form.Group>
+      <Form.Label>Nombre <span>*</span></Form.Label>
+      <Form.Control 
+        // as={}
+        // type=''
+        // placeholder={}
+        // minLength={}
+        // maxLength={}
+        // min={}
+        // max={}
+        // name={}
+        // value={}
+        // onChange={}
+        // required
+      />
+    </Form.Group>
+  )
+}
+
+export default Input
