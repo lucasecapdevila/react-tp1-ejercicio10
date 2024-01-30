a30c7bf0c24508103b67b7a80ecdfa0576e2031e
Lucas Capdevila <lcapdevila60@gmail.com>
2024-01-30 20:09:47 -0300

    se completa componente Formulario y se crea componente Input

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Button, Card, Form } from 'react-bootstrap'
 
 const Formulario = () => {
   return (
-    <div>Formulario</div>
+    <section>
+      <Card>
+        <Card.Header as='h2' className='fw-bold fs-4 px-2'>Ingresa los datos de la película</Card.Header>
+        <Card.Body className='d-flex flex-column'>
+          <Form>
+            <Button type='submit' variant='success' className='mt-2 align-self-end'>Agregar película</Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </section>
   )
 }
 
