2ab4d4b235e9bf697500db78a1e2871c3b11d65f
Lucas Capdevila <lcapdevila60@gmail.com>
2024-01-31 19:02:27 -0300

    se elimina el componente Input para trabajar los inputs directamente en el formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Button, Card, Form } from 'react-bootstrap'
-import Input from './Input'
 
 const Formulario = () => {
   return (
@@ -9,16 +8,29 @@ const Formulario = () => {
         <Card.Header as='h2' className='fw-bold fs-4 px-2'>Ingresa los datos de la película</Card.Header>
         <Card.Body>
           <Form className='d-flex flex-column'>
-            <Input />
-            <Input />
-            <Input />
-            <Form.Select aria-label='Select'>
-              <option>Elija una opción</option>
-              <option value="1">asd1</option>
-              <option value="2">asd2</option>
-              <option value="3">asd3</option>
-              <option value="4">asd4</option>
-            </Form.Select>
+            <Form.Group controlId=''>
+              <Form.Label>Nombre</Form.Label>
+              <Form.Control />
+            </Form.Group>
+            
+            <Form.Group controlId=''>
+              <Form.Label>Descripción</Form.Label>
+              <Form.Control 
+                as='textarea'
+              />
+            </Form.Group>
+
+            <Form.Group controlId=''>
+              <Form.Label>3</Form.Label>
+              <Form.Select aria-label='Select'>
+                <option>Elija una opción</option>
+                <option value="1">asd1</option>
+                <option value="2">asd2</option>
+                <option value="3">asd3</option>
+                <option value="4">asd4</option>
+              </Form.Select>
+            </Form.Group>
+            
             <Button type='submit' variant='success' className='mt-2 align-self-end'>Agregar película</Button>
           </Form>
         </Card.Body>
