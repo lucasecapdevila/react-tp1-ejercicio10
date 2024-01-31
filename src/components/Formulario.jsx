28c2bf1213696a311d3c5b25666d7fda17228f82
Lucas Capdevila <lcapdevila60@gmail.com>
2024-01-31 19:19:47 -0300

    se completa maquetado del formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -8,30 +8,37 @@ const Formulario = () => {
         <Card.Header as='h2' className='fw-bold fs-4 px-2'>Ingresa los datos de la película</Card.Header>
         <Card.Body>
           <Form className='d-flex flex-column'>
-            <Form.Group controlId=''>
+            <Form.Group controlId='nombre'>
               <Form.Label>Nombre</Form.Label>
-              <Form.Control />
+              <Form.Control
+                type='text'
+                placeholder='Nombre de la película'
+                minLength={2}
+                maxLength={50}
+                name='nombre'
+                // value=''
+                required
+              />
             </Form.Group>
             
-            <Form.Group controlId=''>
+            <Form.Group controlId='descripcion'>
               <Form.Label>Descripción</Form.Label>
               <Form.Control 
                 as='textarea'
               />
             </Form.Group>
 
-            <Form.Group controlId=''>
-              <Form.Label>3</Form.Label>
-              <Form.Select aria-label='Select'>
+            <Form.Group controlId='genero'>
+              <Form.Label>Género</Form.Label>
+              <Form.Select aria-label='opciones'>
                 <option>Elija una opción</option>
-                <option value="1">asd1</option>
-                <option value="2">asd2</option>
-                <option value="3">asd3</option>
-                <option value="4">asd4</option>
+                <option value="1">Comedia</option>
+                <option value="2">Drama</option>
+                <option value="3">Infantil</option>
               </Form.Select>
             </Form.Group>
             
-            <Button type='submit' variant='success' className='mt-2 align-self-end'>Agregar película</Button>
+            <Button type='submit' variant='success' className='mt-3 align-self-end'>Agregar película</Button>
           </Form>
         </Card.Body>
       </Card>
