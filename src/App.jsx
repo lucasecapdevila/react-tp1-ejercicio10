ed03eb68ba2b8d34e794ce8832e0776de111f2b7
Lucas Capdevila <lcapdevila60@gmail.com>
2024-02-05 18:50:42 -0300

    se agregan hooks efect y state y se crea lista en Local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,25 @@ import './App.css'
 import Footer from './components/Footer'
 import Formulario from './components/Formulario'
 import ListaPeliculas from './components/ListaPeliculas'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const listaPeliculasLS = JSON.parse(localStorage.getItem('peliculas')) || []
+  const [listaPeliculas, setListaPeliculas] = useState(listaPeliculasLS)
+
+  useEffect(() => {
+    localStorage.setItem('peliculas', JSON.stringify(listaPeliculas))
+  }, [listaPeliculas])
+
   return (
     <>
       <main className='my-4 mainPage'>
         <Container>
           <h1 className='text-center display-3'>Alta de Películas</h1>
           <Formulario />
-          <ListaPeliculas />
+          <ListaPeliculas
+            peliculasProp={listaPeliculas}
+          />
         </Container>
       </main>
 
